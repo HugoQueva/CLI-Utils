074eaa90e6aa2aa02b5f25d43065c5af05134141
HugoQuevaReborn <quevahugo5@gmail.com>
2024-03-14 12:22:20 +0100

    Removed current `clear.rs` implementation, marked as non-implemented.

--- src/clear.rs
+++ src/clear.rs
@@ -1,10 +1,12 @@
 use crate::command::{CommandError, CommandResult};
 
-
-
 pub fn clear() -> Result<CommandResult, CommandError> {
+    // ! FIXME: This implementation does not clear the terminal,
+    // ! but changes the cursor position to the beginning of the terminal.
+    /*
     print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
     println!("\0");
+    */
 
-    Ok(CommandResult::with_empty_text())
+    Err(CommandError::from_str("This command is not implemented yet!"))
 }
