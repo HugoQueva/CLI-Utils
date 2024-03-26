1a9b3c3a0e172cde17963a1962b951e700d203d9
HugoQuevaReborn <quevahugo5@gmail.com>
2024-03-26 22:24:43 +0100

    Removed useless macro `echo!()`

--- src/echo.rs
+++ src/echo.rs
@@ -1,32 +1,5 @@
 use crate::command::{Command, CommandResult, CommandError};
 
-// ! Could of just use a println! macro, but I wanted to keep the code clean
-// ! and experience with macros.
-/// Print a string to the terminal output.
-/// 
-///  # Exemples
-/// 
-/// ```
-/// echo!("Hello, world!");
-/// ```
-/// 
-/// ```
-/// echo!(String::from("Hello, world!"));
-/// ```
-/// 
-///  # Errors
-/// 
-/// Will return an error or an empty string if the input parameter 
-/// is not a valid **`String`** or **`&str`**
-/// 
-///  # Note
-/// This macro does not support the **``std::fmt::Debug``** trait.
-macro_rules! echo {
-    ($x: expr) => {
-        println!("{}", $x);
-    };
-}
-
 pub fn echo(command: Command) -> Result<CommandResult, CommandError> {
     let args = command.args.split_whitespace().collect::<Vec<&str>>();
 
@@ -44,7 +17,7 @@ pub fn echo(command: Command) -> Result<CommandResult, CommandError> {
         echo_string.push_str(&format!("{} ",arg));
     }
 
-    echo!(echo_string);
+    println!("{echo_string}");
 
     Ok(CommandResult::with_empty_text())
 }
